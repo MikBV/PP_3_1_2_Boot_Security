c2795cb9c7e10f410f858384d5c6775684080703
MikBV <bmv00791@gmail.com>
2024-12-05 07:57:30 +0800

    feat: new structure transfer classes from previous project

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -0,0 +1,89 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.Optional;
+
+@Controller
+public class UsersController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = "/index")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
+    }
+
+
+    @GetMapping(value = "/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
+    }
+
+    /**
+     * Маппинги для добавляения нового пользователя
+     */
+
+    @GetMapping(value = "/users/new")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping(value = "/users/new")
+    public String saveUser(@ModelAttribute User user) {
+        userRepository.save(user);
+        return "changeconfirm";
+    }
+
+    /**
+     * Мапы для изменения пользователя
+     */
+
+    @GetMapping(value = "/users/change/id")
+    public String changeUserById(@RequestParam(value = "id", required = true) Long id, Model model) {
+        Optional<User> optiUser = userRepository.findById(id);
+        User user = optiUser.orElse(null);
+        model.addAttribute("user", user);
+        return "change";
+    }
+
+    @PostMapping(value = "/users/change/id")
+    public String saveChangesInUser(@ModelAttribute User user, @RequestParam Long id) {
+        userRepository.save(user);
+        return "changeconfirm";
+    }
+
+    /**
+     * Маппинги для удаления
+     */
+
+    @GetMapping(value = "/users/delete/id")
+    public String deleteUserById(@RequestParam(value = "id", required = true) Long id, Model model) {
+        Optional<User> optiUser = userRepository.findById(id);
+        User user = optiUser.orElse(null);
+        model.addAttribute("user", user);
+        return "delete";
+    }
+
+    @PostMapping(value = "/users/delete/id")
+    public String deletingUserById(@RequestParam(value = "id", required = true) Long id) {
+        userRepository.deleteById(id);
+        return "changeconfirm";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -0,0 +1,104 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Класс-сущность User
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+    /**
+     * Поля сущности, 4 штуки + автогенерирующийся айдишник
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="username",unique=true, nullable=false, length=64)
+    private String username;
+
+    @Column(name="password",nullable = false, length=24)
+    private String password;
+
+    @Column(name="firstname",nullable=false, length=32)
+    private String firstName;
+
+    @Column(name="lastname",nullable=false, length=32)
+    private String lastName;
+
+    /**
+     * Конструкторы
+     */
+
+    public User() {
+    }
+
+    public User(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Геттеры и сеттеры
+     */
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Переопределённый toString
+     */
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                "}\n";
+    }
+}
